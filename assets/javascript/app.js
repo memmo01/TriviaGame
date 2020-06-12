a635777612ccd369db822e507aa4f58c9bad344b
michael emmons <memmons18@gmail.com>
2020-06-12 00:12:23 -0400

    updated background color and changed box layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,164 +1,193 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var panel = $("#quiz");
+  var num = 15;
 
-var panel=$("#quiz");
-var num = 15;
+  var questions = [
+    {
+      question: "what is marios brothers name?",
+      answers: ["luigi", "bowser", "toad", "yoshi"],
+      correctAnswer: "luigi",
+      image: "assets/images/luigi.png",
+    },
+    {
+      question:
+        'who played the mario character in the 1993 movie "super mario bros"',
+      answers: ["John Candy", "Joe Pesci", "John Leguizamo", "Bob Hoskins"],
+      correctAnswer: "Bob Hoskins",
+      image: "assets/images/bob.jpg",
+    },
+    {
+      question: "What was mario's original name?",
+      answers: ["Starboy", "Red-Plunger", "Maurice", "Jumpman"],
+      correctAnswer: "Jumpman",
+      image: "assets/images/jumpman.gif",
+    },
+    {
+      question: "What is nintendo's best selling mario game?",
+      answers: [
+        "super mario bros",
+        "mario kart 7",
+        "super mario galaxy",
+        "super mario 64",
+      ],
+      correctAnswer: "super mario bros",
+      image: "assets/images/mario1.png",
+    },
+    {
+      question: "where does mario live?",
+      answers: ["Tokyo", "New York City", "Mushroom Kingdom", "under a bridge"],
+      correctAnswer: "Mushroom Kingdom",
+      image: "assets/images/mushkingg.JPG",
+    },
+  ];
 
-var questions = [{
-	question:"what is marios brothers name?",
-	answers: ["luigi","bowser","toad","yoshi"],
-	correctAnswer:"luigi",
-	image:"assets/images/luigi.png"
-},{
-	question:'who played the mario character in the 1993 movie "super mario bros"',
-	answers:["John Candy","Joe Pesci","John Leguizamo","Bob Hoskins"],
-	correctAnswer:"Bob Hoskins",
-	image:"assets/images/bob.jpg"
+  var timer;
 
-},{
-	question:"What was mario's original name?",
-	answers:["Starboy","Red-Plunger","Maurice","Jumpman"],
-	correctAnswer:"Jumpman",
-	image:"assets/images/jumpman.gif"
+  var game = {
+    questions: questions,
+    currentQuestion: 0,
+    counter: num,
+    correct: 0,
+    incorrect: 0,
 
-},
-{
-	question:"What is nintendo's best selling mario game?",
-	answers:["super mario bros","mario kart 7","super mario galaxy","super mario 64"],
-	correctAnswer:"super mario bros",
-	image:"assets/images/mario1.png"
+    countdown: function () {
+      game.counter--;
+      $("#timers").html(game.counter);
+      if (game.counter === 0) {
+        console.log("times UP");
+        game.timeUp();
+      }
+    },
+    loadQuestion: function () {
+      timer = setInterval(game.countdown, 1000);
+      panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 
-},
-{
-	question:"where does mario live?",
-	answers:["Tokyo","New York City","Mushroom Kingdom","under a bridge"],
-	correctAnswer:"Mushroom Kingdom",
-	image:"assets/images/mushkingg.JPG"
+      for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+        panel.append(
+          "<button class='answer-button' id='button' data-name='" +
+            questions[this.currentQuestion].answers[i] +
+            "'>" +
+            questions[this.currentQuestion].answers[i] +
+            "</button>"
+        );
+      }
+    },
+    nextQuestion: function () {
+      game.counter = num;
+      $("#timers").html(game.counter);
+      game.currentQuestion++;
+      game.loadQuestion();
+    },
 
+    timeUp: function () {
+      clearInterval(timer);
 
-}];
+      $("#timers").html(game.counter);
 
-var timer;
+      panel.html("<h2>Out of time!</h2>");
+      panel.append(
+        "<h3>The correct answer was: " +
+          questions[this.currentQuestion].correctAnswer
+      );
+      panel.append(
+        "<img src='" + questions[this.currentQuestion].image + "'/>"
+      );
 
+      if (game.currentQuestion === questions.length - 1) {
+        setTimeout(game.results, 3 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 3 * 1000);
+      }
+    },
 
-var game={
-	questions:questions,
-	currentQuestion:0,
-	counter:num,
-	correct:0,
-	incorrect:0,
+    results: function () {
+      clearInterval(timer);
 
-	countdown:function(){
-		game.counter--;
-		$("#timers").html(game.counter);
-		if(game.counter === 0){
-			console.log("times UP");
-			game.timeUp();
-		}
-	},
-	loadQuestion:function(){
-		timer=setInterval(game.countdown,1000);
-		panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>")
-		
-		for(var i=0;i<questions[this.currentQuestion].answers.length; i++){
-			panel.append("<button class='answer-button' id='button' data-name='"
-				+questions[this.currentQuestion].answers[i] + "'>" + 
-				questions[this.currentQuestion].answers[i] + "</button>");
-		}
-	},
-	nextQuestion: function(){
-		game.counter = num;
-		$("#timers").html(game.counter);
-		game.currentQuestion++;
-		game.loadQuestion();
-	},
+      panel.html("<h2>All done, heres how you did!</h2>");
 
-	timeUp: function(){
-		clearInterval(timer);
+      $("#timers").html(game.counter);
 
-		$("#timers").html(game.counter);
+      panel.append("<h3> Correct Answers: " + game.correct + "<h3>");
+      panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+      panel.append(
+        "<h3>Unanswered: " +
+          (questions.length - (game.incorrect + game.correct)) +
+          "</h3>"
+      );
+      panel.append("<br><button id='start-over'>Start Over?</button>");
+    },
+    clicked: function (e) {
+      clearInterval(timer);
+      if (
+        $(e.target).attr("data-name") ===
+        questions[this.currentQuestion].correctAnswer
+      ) {
+        this.answeredCorrectly();
+      } else {
+        this.answeredIncorrectly();
+      }
+    },
 
-		panel.html("<h2>Out of time!</h2>");
-		panel.append("<h3>The correct answer was: " +questions[this.currentQuestion].correctAnswer);
-		panel.append("<img src='" + questions[this.currentQuestion].image + "'/>");
-		
-		if (game.currentQuestion === questions.length -1){
-			setTimeout(game.results, 3 *1000);
-		}
-		else{
-			setTimeout(game.nextQuestion,3*1000);
-		}
-	},
+    answeredIncorrectly: function () {
+      game.incorrect++;
+      clearInterval(timer);
 
-	results: function(){
-		clearInterval(timer);
+      panel.html("<h2>Incorrect!</h2>");
+      panel.append(
+        "<h3> The Correct Answer was: " +
+          questions[game.currentQuestion].correctAnswer +
+          "</h3>"
+      );
+      panel.append(
+        "<img src=" +
+          questions[game.currentQuestion].image +
+          " height='150px'/>"
+      );
 
-		panel.html("<h2>All done, heres how you did!</h2>");
+      if (game.currentQuestion === questions.length - 1) {
+        setTimeout(game.results, 3 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 3 * 1000);
+      }
+    },
+    answeredCorrectly: function () {
+      clearInterval(timer);
+      game.correct++;
+      panel.html("<h2>correct</h2>");
+      panel.append(
+        "<img src=" +
+          questions[game.currentQuestion].image +
+          " height='150px'/>"
+      );
 
-$("#timers").html(game.counter);
+      if (game.currentQuestion === questions.length - 1) {
+        setTimeout(game.results, 3 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 3 * 1000);
+      }
+    },
 
-panel.append("<h3> Correct Answers: " + game.correct + "<h3>");
-panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
-panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) +"</h3>");
-panel.append("<br><button id='start-over'>Start Over?</button>");
-},
-	clicked: function(e){
-		clearInterval(timer);
-		if($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer){
-			this.answeredCorrectly();
-		}
-		else{
-			this.answeredIncorrectly();
-		}
-	},
+    reset: function () {
+      this.currentQuestion = 0;
+      this.counter = num;
+      this.correct = 0;
+      this.incorrect = 0;
+      this.loadQuestion();
+    },
+  };
 
- 	answeredIncorrectly: function(){
- 		game.incorrect++;
- 		clearInterval(timer);
+  $(document).on("click", "#start-over", function () {
+    game.reset();
+  });
+  $(document).on("click", ".answer-button", function (e) {
+    game.clicked(e);
+  });
+  $(document).on("click", "#start", function () {
+    $("#timer").prepend(
+      "<h2>Time remaining: <span id='timers'>15</span> Seconds</h2>"
+    );
+    $(".infoDisplay").css("background", "rgba(255,255,255,.9");
 
- 		panel.html("<h2>Incorrect!</h2>");
- 		panel.append("<h3> The Correct Answer was: " + questions[game.currentQuestion].correctAnswer+"</h3>");
- 		panel.append("<img src=" + questions[game.currentQuestion].image +" height='150px'/>");
-
- 		if(game.currentQuestion === questions.length -1){
- 			setTimeout(game.results, 3 *1000);
- 		}
- 		else{
- 			setTimeout(game.nextQuestion, 3 * 1000);
- 		}
-},
-	answeredCorrectly: function(){
-		clearInterval(timer);
-		game.correct++;
-		panel.html("<h2>correct</h2>");
-		panel.append("<img src="+questions[game.currentQuestion].image + " height='150px'/>");
-		
-		if(game.currentQuestion === questions.length -1){
-			setTimeout(game.results, 3 * 1000);
-		}
-		else{
-			setTimeout(game.nextQuestion,3 * 1000);
-		}
-	},
-
-	reset: function(){
-		this.currentQuestion = 0;
-		this.counter = num;
-		this.correct =0;
-		this.incorrect =0;
-		this.loadQuestion();
-	}
-};
-
-$(document).on("click", "#start-over", function(){
-	game.reset();
+    game.loadQuestion();
+  });
 });
-$(document).on("click", ".answer-button", function(e){
-	game.clicked(e);
-});
-$(document).on("click", "#start", function(){
-
-	$("#timer").prepend("<h2>Time remaining: <span id='timers'>15</span> Seconds</h2>");
-	$(".infoDisplay").css('background','#FFD972');
-	
-	game.loadQuestion();
-});})
